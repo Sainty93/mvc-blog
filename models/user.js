ebddee31d0d3dfcaef59c32981f8c0cc13a3069c
Sainty93 <jenandfloyd93@icloud.com>
2023-03-20 12:12:38 -0700

    finished user js

--- models/user.js
+++ models/user.js
@@ -1,2 +1,47 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize 
+const sequelize = require('../config/connection');
+
+const bcrypt =require('bcrypt');
+
+class user extends model {}
+
+user.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: { 
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { len: [8] },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await
+                bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+        },
+        beforeUpdate: async (updateUserData) => {
+            updateUserData.password = await bcrypt.hash(
+                updateUserData.password,
+                10
+            );
+            return updateUserData;
+        },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
